98ccb28278fa4664a5554c6ff56be2dcbeb3cca5
Leonid Petrov <189938948+MySelfZalin@users.noreply.github.com>
2026-07-21 03:52:50 +0300

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        association = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+        for char in s:
+            if char not in association:
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+
+                if association[char] != stack.pop():
+                    return False
+
+        return not stack                  
+        
